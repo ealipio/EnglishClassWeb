c63abf3ae37951165979d0a2f34bcc7fbe4d78ba
oa4ajp <mendiola_loyola@yahoo.com>
2018-01-17 09:50:31 -0500

    PBI - CrearGruposDeEstudioProfesor

--- src/app/teacher-schedule/teacher-edit-schedule/teacher-edit-schedule.component.ts
+++ src/app/teacher-schedule/teacher-edit-schedule/teacher-edit-schedule.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageBox } from '../../common/models/message-box';
+import { ActivatedRoute } from '@angular/router';
+import { TeacherScheduleService } from '../services/teacher-schedule.service';
 
 @Component({
   selector: 'app-teacher-edit-schedule',
   templateUrl: './teacher-edit-schedule.component.html',
   styleUrls: ['./teacher-edit-schedule.component.scss']
 })
-export class TeacherEditScheduleComponent implements OnInit {
-  teacherName = 'GiomaraAndre Arce';
+export class TeacherEditScheduleComponent extends MessageBox implements OnInit {
+  teacherId: number;
+  teacher = null;
+  scheduleList = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private teacherScheduleService: TeacherScheduleService) { 
+    super();
+    this.teacherId = this.route.snapshot.params['id'];
+    this.getTeacherById(this.teacherId);
+    this.getSchedulesByTeacher(this.teacherId);
+  }
 
   ngOnInit() {
+  }
+
+  private getTeacherById(teacherId: number) {
+    this.teacherScheduleService.getTeacher(teacherId).subscribe(
+      result => {
+        this.teacher = result;        
+      }, error => {
+        this.message = `Error: ${error.statusText}`;
+        this.messageType = 'error';
+        this.showMessage = true;
+      });
+  }
+
+  private getSchedulesByTeacher(teacherId: number) {
+    this.teacherScheduleService.getSchedulesByTeacher(teacherId).subscribe(
+      result => {
+        this.scheduleList = result;        
+      }, error => {
+        this.message = `Error: ${error.statusText}`;
+        this.messageType = 'error';
+        this.showMessage = true;
+      });
   }
 
 }
