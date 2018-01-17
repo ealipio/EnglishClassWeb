a464583edc6dc999913fb07978413dee17653c48
ryarleque <renzoyarleque@gmail.com>
2018-01-17 09:43:16 -0500

    Implemented dropdown validations

--- src/app/teacher-schedule/teacher-edit-schedule/teacher-edit-schedule.component.ts
+++ src/app/teacher-schedule/teacher-edit-schedule/teacher-edit-schedule.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-edit-schedule.component.scss']
 })
 export class TeacherEditScheduleComponent implements OnInit {
-  teacherName = 'GiomaraAndre Arce';
+  teacherName : string;
+  isDropdownVisible: boolean;
+  scheduleValue : string;
+  levelValue : string;
 
-  constructor() { }
+  constructor() { 
+    this.teacherName = 'GiomaraAndre Arce';
+    this.scheduleValue = 'Select Schedule';
+    this.levelValue = 'Select Level';
+    this.isDropdownVisible = false;
+  }
 
   ngOnInit() {
+  }
+
+  showDropdown () {
+    this.isDropdownVisible = true;
+  }
+
+  setSchedule (schedule:string) {
+    this.scheduleValue = schedule;
+    this.isDropdownVisible = false;
+  }
+
+  setLevel (level:string) {
+    this.levelValue = level;
+    this.isDropdownVisible = false;
   }
 
 }
